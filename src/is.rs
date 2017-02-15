c9a38aecbf7402b8e1d3ef3a4940ad53d42650a1
Karoline Plum <karoline@plum-ge.de>
2017-02-15 13:33:38 +0100

    Create enum Command representing all instructions that the assembler can handle
    Can be created from a string and can be converted into opcode

---
+++ src/is.rs
@@ -0,0 +1,29 @@
+pub enum Command {
+   Addu,
+   Addui,
+   Cmpu,
+   Cmpui,
+}
+
+impl Command {
+    pub fn from_str(s: &str) -> Option<Self> {
+        use self::Command::*;
+        match s {
+            "ADDU" => Some(Addu),
+            "ADDUI" => Some(Addui),
+            "CMPU" => Some(Cmpu),
+            "CMPUI" => Some(Cmpui),
+            _ => None,
+        }
+    }
+
+    pub fn opcode(&self) -> u8 {
+        use self::Command::*;
+        match *self {
+            Addu => 0x22,
+            Addui => 0x23,
+            Cmpu => 0x32,
+            Cmpui => 0x33,
+        }
+    }
+}
